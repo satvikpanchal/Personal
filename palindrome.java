fce0feb6d9a81c33cb7f771e3a606a8812aaa35b
satvikpanchal <85001040+satvikpanchal@users.noreply.github.com>
2022-02-21 06:32:41 -0700

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,35 @@
+//package Personal;
+
+import java.util.Scanner;
+
+public class palindrome
+{
+    public static void main(String[] args)
+    {
+        String str;
+
+        StringBuilder sb = new StringBuilder();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What string should I check?");
+        str = input.nextLine();
+
+        StringBuilder sb2 = new StringBuilder(str);
+
+        char list[] = str.toCharArray();
+
+        for(int i = (list.length - 1); i >= 0; i--)
+        {
+            sb.append(list[i]);
+        }
+
+        if(sb2.toString().equals(sb.toString()))
+        {
+            System.out.println(str + " is a palindrome.");
+        }
+        else
+            System.out.println(str + " is not a palindrome.");
+    }
+
+}
